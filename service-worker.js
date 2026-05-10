62083c5cb311d3214affaab2e3685e2109a3aef6
songzue <czixi533@gmail.com>
2026-05-10 18:21:33 +0800

    Fix service worker - runtime cache

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,20 @@
-const CACHE = 'days-v1';
-const FILES = ['/', '/index.html'];
+const CACHE = 'days-v2';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      return caches.open(CACHE).then(c => {
+        c.put(e.request, res.clone());
+        return res;
+      });
+    }))
   );
 });
